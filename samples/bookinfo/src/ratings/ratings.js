f3e1052cbb8bda7062d5bda51cc955b0047460e8
birdietiger <github@birdsall.org>
2019-05-20 14:51:50 -0400

    add appd ratings and product page

--- samples/bookinfo/src/ratings/ratings.js
+++ samples/bookinfo/src/ratings/ratings.js
@@ -12,7 +12,12 @@
 //   See the License for the specific language governing permissions and
 //   limitations under the License.
 
-require("appdynamics");
+require("appdynamics").profile({
+  logging: {
+    'level': 'INFO',
+    'outputType': 'console'
+  }
+});
 
 var http = require('http')
 var dispatcher = require('httpdispatcher')
